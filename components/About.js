188f5ef1ac0c5b74d21ac6199cc670dcf1987a08
jolmstead <jl260629@dal.ca>
2021-11-24 22:26:11 -0400

    initial commit

---
+++ components/About.js
@@ -0,0 +1,39 @@
+import React from "react"
+import Image from "next/image"
+import profile from "../public/profile.jpeg"
+
+const About = () => {
+    return (
+        <div className="container px-4 mx-auto">
+            <div className="lg:space-x-5 lg:flex lg:flex-row item-center lg:-mx-4 flex flex-col-reverse text-center lg:text-left">
+                <div className="lg:px-4 lg:mt-12 ">
+                    <h1 className="text-2xl font-bold text-gray-900 lg:text-5xl dark:text-white">
+                        Hey there,
+                    </h1>
+                    <div className="mt-6 text-gray-800 dark:text-white">
+                        <p className="mb-4">
+                            My name is <bold>Julia Olmstead</bold> and I am a developer and passionate designer with global entreprenurial experience. 
+                            My passion for creation led me to my start of programming. I create 2D platformers, apps, and websites (obviously). 
+                            Currently I'm learning how to code in Java, JavaScript, Python, and more through various university classes. 
+                            In my free time I like to learn C# and react frameworks. 
+                            
+                        </p>
+                    </div>
+                </div>
+                <div className="flex-shrink-0 lg:mt-12 lg:px-4 mb-10">
+                    <Image
+                        src={profile}
+                        alt="Profile"
+                        priority={true}
+                        className="rounded-full"
+                        width={250}
+                        height={250}
+                        placeholder="blur"
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About;
